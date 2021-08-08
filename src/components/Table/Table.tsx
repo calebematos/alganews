1d3a9a2944610a705937ca3c21a2dbd0dc040815
Lucas Calebe <lucas.lopes@integritas.net>
2021-08-07 22:12:26 -0300

    Making table reusable

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,80 +1,10 @@
-import { mdiOpenInNew } from "@mdi/js";
-import Icon from "@mdi/react";
-import { useMemo } from "react";
-import { Column, useTable } from "react-table"
+import { TableInstance } from "react-table"
 import * as T from './Table.styles'
 
-type Data = {
-  preview: React.ReactNode
-  col1: string
-  col2: string
-  actions: string
-}
 
-export default function Table(){
+export default function Table<T extends Object>({ instance }: 
+    {instance: TableInstance<T>}){
 
-  const data = useMemo<Data[]>(
-    () => [
-      {
-        col1: 'Hello',
-        col2: 'World',
-        actions: 'ações',
-        preview: <Icon 
-          size= "14px"
-          color= "#09f"
-          path= {mdiOpenInNew}
-        />
-      },
-      {
-        col1: 'react-table',
-        col2: 'rocks',
-        actions: 'ações',
-        preview: <Icon 
-          size= "14px"
-          color= "#09f"
-          path= {mdiOpenInNew}
-        />
-      },
-      {
-        col1: 'whatever',
-        col2: 'you want',
-        actions: 'ações',
-        preview: <Icon 
-          size= "14px"
-          color= "#09f"
-          path= {mdiOpenInNew}
-        />
-      },
-    ],
-    []
-  )
-
-  const columns = useMemo<Column<Data>[]>(
-    () => [
-      {
-        Header: '',
-        accessor: 'preview',
-      },
-      {
-        Header: 'Column 1',
-        accessor: 'col1',
-        width: 320,
-        Cell(row){
-        return <div style={{textAlign:'center'}}>{row.value}</div>
-        }
-        
-      },
-      {
-        Header: 'Column 2',
-        accessor: 'col2',
-      },
-      {
-        Header: 'Ações',
-        accessor: 'actions',
-      },
-    ],
-    []
-  )
 
   const {
     getTableProps,
@@ -82,7 +12,7 @@ export default function Table(){
     headerGroups,
     rows,
     prepareRow,
-  }= useTable({ columns, data })
+  }= instance
 
   return (
     <T.Wrapper cellPadding={0} cellSpacing={0} {...getTableProps()}>
--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -1,14 +1,166 @@
-import { Story, Meta } from '@storybook/react';
+import { mdiOpenInNew } from '@mdi/js';
+import { Meta } from '@storybook/react';
+import { useMemo } from 'react';
+import Icon from "@mdi/react";
 
 import  Table  from '../components/Table/Table';
+import { Column, useTable } from 'react-table';
 
 export default {
   title: 'Example/Table',
   component: Table,
 } as Meta;
 
-const Template: Story<{}> = (args) => <Table {...args} />;
+type Post = {
+  id: number
+  title: string
+  views: number
+  author: {
+    name: string
+    avatar: string
+  }
+  conversions: {
+    thousands: number
+    percentage: number
+  }
+}
 
-export const Default = Template.bind({})
-Default.args ={
- }
+
+export function Default() {
+  
+
+  const data = useMemo<Post[]>(
+    () => [
+      {
+        author: {
+          name: 'Daniel Bonifacio',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+        },
+        id: 1,
+        conversions: {
+          percentage: 64.35,
+          thousands: 607,
+        },
+        title: 'Como dobrei meu salário aprendendo somente React',
+        views: 985415
+      },
+      {
+        author: {
+          name: 'Daniel Bonifacio',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+        },
+        id: 2,
+        conversions: {
+          percentage: 64.35,
+          thousands: 607,
+        },
+        title: 'React.js vs. React Native: a REAL diferença entre os dois',
+        views: 985415
+      },
+      {
+        author: {
+          name: 'Daniel Bonifacio',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+        },
+        id: 3,
+        conversions: {
+          percentage: 95.35,
+          thousands: 845,
+        },
+        title: 'Como dobrei meu salário aprendendo somente React',
+        views: 985415
+      }
+    ],
+    []
+  )
+
+  const columns = useMemo<Column<Post>[]>(
+    () => [
+      {
+        Header: '',
+        accessor: 'id',
+        Cell: () =>  <Icon size= {'14px'} color= {'#09f'} path= {mdiOpenInNew} />
+
+      },
+      {
+        Header: () => <div style={{ textAlign:'left' }}>Artigo</div>,
+        accessor: 'title',
+        width: 320,
+        Cell: (props) => <div style={{textAlign:'left', display:'flex', alignItems:'center', gap:8}}>
+          <img width={24} height={24} src={props.row.original.author.avatar} alt={props.row.original.author.name}/>
+          {props.value}
+        </div>        
+      },
+      {
+        Header: () => <div style={{ textAlign:'right' }}>Views</div>,
+        accessor: 'views',
+        Cell: (props) => <div style={{textAlign:'right', fontWeight: 700, fontFamily: '"Roboto mono", monospace' }}>
+          {props.value.toLocaleString('pt-BR')}
+        </div>
+      },
+      {
+        Header: () => <div style={{ textAlign:'right' }}>Conversões</div>,
+        accessor: 'conversions',
+        Cell: (props) => <div style={{display:'flex', gap:8, fontWeight: 700, fontFamily: '"Roboto mono", monospace' }}>
+          <span>{props.value.thousands}k</span>
+          <span style={{ color:'#09f' }}>({props.value.percentage}%)</span>
+        </div>        
+      },
+    ],
+    []
+  )
+  
+  const instance = useTable<Post>({ data, columns })
+
+  return <Table<Post> instance={instance}/>
+}
+
+
+export function NoData() {
+  
+
+  const data = useMemo<Post[]>(
+    () => [ ],
+    []
+  )
+
+  const columns = useMemo<Column<Post>[]>(
+    () => [
+      {
+        Header: '',
+        accessor: 'id',
+        Cell: () =>  <Icon size= {'14px'} color= {'#09f'} path= {mdiOpenInNew} />
+
+      },
+      {
+        Header: 'Artigo',
+        accessor: 'title',
+        width: 320,
+        Cell: (props) => <div style={{textAlign:'left', display:'flex', alignItems:'center', gap:8}}>
+          <img width={24} height={24} src={props.row.original.author.avatar} alt={props.row.original.author.name}/>
+          {props.value}
+        </div>        
+      },
+      {
+        Header: 'Views',
+        accessor: 'views',
+        Cell: (props) => <div style={{textAlign:'right', fontWeight: 700, fontFamily: '"Roboto mono", monospace' }}>
+          {props.value.toLocaleString('pt-BR')}
+        </div>
+      },
+      {
+        Header: 'Conversões',
+        accessor: 'conversions',
+        Cell: (props) => <div style={{display:'flex', gap:8, fontWeight: 700, fontFamily: '"Roboto mono", monospace' }}>
+          <span>{props.value.thousands}k</span>
+          <span style={{ color:'#09f' }}>({props.value.percentage}%)</span>
+        </div>        
+      },
+    ],
+    []
+  )
+  
+  const instance = useTable<Post>({ data, columns })
+
+  return <Table<Post> instance={instance}/>
+}
