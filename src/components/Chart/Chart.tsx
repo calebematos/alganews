e4bfdeb57565136bcbd87c8451c66ba5ce236f8b
Lucas Calebe <lucas.lopes@integritas.net>
2021-08-12 21:53:23 -0300

    Add chart component

---
+++ src/components/Chart/Chart.tsx
@@ -0,0 +1,74 @@
+import { transparentize } from 'polished';
+import { Line } from 'react-chartjs-2';
+import styled from 'styled-components';
+import Heading from '../Typography/Heading'
+import NoData from '../NoData/NoData';
+
+const options: Chart.ChartOptions = {
+  maintainAspectRatio: true,
+  elements: {
+    line: {
+      tension:0
+    }
+  },
+  legend: {
+    display: true,
+    align: 'center',
+    position: 'bottom',
+    labels: {
+      usePointStyle: true
+    }
+    
+  },
+  scales: {
+    xAxes:[
+      {
+        display: true,
+        gridLines: {
+          display: false
+        }
+      }
+    ],
+    yAxes: [
+      {
+        type: 'linear',
+        display: false,
+        position: 'left',
+        id: 'cashflow',
+      }
+    ],
+  },
+};
+
+export interface ChartProps {
+  data: Chart.ChartData,
+  title: string
+}
+
+export default function Chart({ data, title }: ChartProps){
+  return <ChatWrapper style={{width:640}}>
+    <div style={{ marginBottom: 16}}>
+      <Heading level={3}>
+        {title}
+      </Heading>
+      
+    </div>
+    {
+      data 
+      ? <Line 
+        height={139}
+        width={600}
+        type="line"
+        data={data} 
+        options={options}
+        />    
+      : <NoData heigh={139}/>
+    }
+  </ChatWrapper>
+}
+
+const ChatWrapper = styled.div`
+  text-align: center;
+  padding: 20px;
+  border: 1px solid ${transparentize(0.9, '#274060')};
+`
---
+++ src/stories/Chart.stories.tsx
@@ -0,0 +1,49 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import  Chart  from '../components/Chart/Chart';
+
+
+const data: Chart.ChartData = {
+  labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
+  datasets: [
+    {
+      label: 'Desempenho pessoal',
+      data: [120, 190, 300, 550, 212, 375],
+      fill: true,
+      backgroundColor: '#0099FF',
+      borderColor: '#0099FF',
+      borderWidth: 0.5,
+      yAxisID: 'cashflow',
+    },
+    {
+      label: 'Média de desempenho do time',
+      data: [100, 220, 150, 640, 371, 563],
+      fill: true,
+      backgroundColor: '#274060',
+      borderColor: '#274060',
+      borderWidth: 0.5,
+      yAxisID: 'cashflow',
+    },
+  ],
+};
+
+
+export default {
+  title: 'Example/Chart',
+  component: Chart
+} as ComponentMeta<typeof Chart>;
+
+
+
+const Template: ComponentStory<typeof Chart> = (args) => <Chart {...args} />;
+
+export const Default = Template.bind({})
+Default.args ={
+  title: 'Média de performance nos últimos 12 meses',
+  data
+}
+
+export const WithoutData = Template.bind({})
+WithoutData.args ={
+  title: 'Média de performance nos últimos 12 meses'
+}
