6d00db3b93ba26dce428a078f8597d08abca1bcd
Lucas Calebe <lucas.lopes@integritas.net>
2021-08-14 15:32:15 -0300

    Add circlechart component

---
+++ src/stories/CircleChart.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import  CircleChart  from '../components/CircleChart';
+
+export default {
+  title: 'Example/CircleChart',
+  component: CircleChart,
+  argTypes: {
+    progress: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100
+      }
+    }
+  }
+} as ComponentMeta<typeof CircleChart>;
+
+const Template: ComponentStory<typeof CircleChart> = (args) => <CircleChart {...args} />;
+
+export const Default = Template.bind({})
+Default.args ={
+  progress: 80,
+  size: 150,
+}
