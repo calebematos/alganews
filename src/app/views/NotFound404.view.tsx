f316c7ac3c82c217a72535189030b4125d4c781f
Lucas Calebe <calebematos@gmail.com>
2021-10-31 21:06:28 -0300

    Updating 404 page

--- src/app/views/NotFound404.view.tsx
+++ src/app/views/NotFound404.view.tsx
@@ -1,8 +1,44 @@
 import usePageTitle from "../../core/hooks/usePageTitle"
+import notFound from "../../assets/not_found.svg"
+import Button from "../components/Button/Button"
+import styled from "styled-components"
+import { useHistory } from "react-router"
 
 export default function NotFound404() {
   usePageTitle("Not found")
-  return <div>
-    <h1>Not Found 404</h1>
-  </div>
+  const history = useHistory()
+
+  return <NotFound404Wrapper>
+    <span>
+      Oops!
+    </span>
+    <h1>We can't find this page</h1>
+    <img src={notFound} alt="Not found" />
+    <Button 
+      variant="primary" 
+      label="Go to home"
+      onClick={() => history.replace('/')}
+    />
+  </NotFound404Wrapper>
 }
+
+const NotFound404Wrapper = styled.div`
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  gap: 32px;
+
+  span {
+    font-size: 72px;
+  }
+
+  h1 {
+    font-size: 18px;
+    font-weight: 400
+  }
+
+`
