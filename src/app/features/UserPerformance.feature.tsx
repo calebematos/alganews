62e0a0076d1b405465aaec17abdd932999504e97
Lucas Calebe <calebematos@gmail.com>
2021-11-04 21:46:57 -0300

    Fixing text

--- src/app/features/UserPerformance.feature.tsx
+++ src/app/features/UserPerformance.feature.tsx
@@ -1,10 +1,10 @@
 import Chart from "../components/Chart/Chart";
 
 const FAKE_DATA = {
-  labels: ['Batata', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
   datasets: [
     {
-      label: 'Receitas',
+      label: 'Revenues',
       data: [500, 400, 600, 100, 800, 20, 123, 320, 120, 500, 434, 322],
       fill: true,
       backgroundColor: '#0099FF',
@@ -13,7 +13,7 @@ const FAKE_DATA = {
       yAxisID: 'cashflow',
     },
     {
-      label: 'Despesas',
+      label: 'Expenses',
       data: [100, 200, 250, 500, 1000, 600, 123, 210, 344, 800, 123, 0],
       fill: true,
       backgroundColor: '#274060',
