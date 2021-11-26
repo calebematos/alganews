b2810741cf115f83f8c2f222e4a1d783d643dcc4
Lucas Calebe <calebematos@gmail.com>
2021-11-26 20:54:47 -0300

    Update top 3 tag

--- src/app/features/UserTopTags.feature.tsx
+++ src/app/features/UserTopTags.feature.tsx
@@ -1,30 +1,34 @@
-import styled from "styled-components"
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Metric } from "../../sdk/@types";
+import MetricService from "../../sdk/service/Metric.service";
 import CircleChart from "../components/CircleChart";
 
 export default function UserTopTags() {
+  const [topTags, setTopTags] = useState<Metric.EditorTagRatio>([]);
 
-  return <UserTopTagsWrapper>
-    <CircleChart 
-      progress={73}
-      size={88}
-      caption={'Java'}
-      theme={'primary'}
-    />
-    <CircleChart 
-      progress={27}
-      size={88}
-      caption={'Javascript'}
-    />
-    <CircleChart 
-      progress={82}
-      size={88}
-      caption={'scrum'}
-    />
-  </UserTopTagsWrapper>
+  useEffect(() => {
+    MetricService.getTop3Tags().then(setTopTags);
+  }, []);
+
+  return (
+    <UserTopTagsWrapper>
+      {topTags.map((tag, i) => {
+        return (
+          <CircleChart
+            caption={tag.tagName}
+            progress={tag.percentage}
+            size={88}
+            theme={i === 0 ? "primary" : "default"}
+          />
+        );
+      })}
+    </UserTopTagsWrapper>
+  );
 }
 
 const UserTopTagsWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(3,1fr);
+  grid-template-columns: repeat(3, 1fr);
   gap: 32px;
-`
+`;
