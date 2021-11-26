09a3f5133c47d981a483b8d2ce9bd7bcfb0746f6
Lucas Calebe <calebematos@gmail.com>
2021-11-26 20:54:56 -0300

    Update top 3 tag

--- src/app/components/CircleChart/CircleChart.tsx
+++ src/app/components/CircleChart/CircleChart.tsx
@@ -1,67 +1,55 @@
-import { useEffect, useState } from 'react'
-import * as CC from './CircleChart.styles'
+import { useEffect, useState } from "react";
+import * as CC from "./CircleChart.styles";
 
 export interface CircleChartProps {
-  progress: number
-  size: number
-  caption?: string
-  theme?: 'default' | 'primary'
-  strokeWidth?: number
+  progress: number;
+  size: number;
+  caption?: string;
+  theme?: "default" | "primary";
+  strokeWidth?: number;
 }
 
-function CircleChart(props : CircleChartProps){
+function CircleChart(props: CircleChartProps) {
+  const getThemeColor = () => (props.theme === "primary" ? "#09f" : "#274060");
 
-  const getThemeColor = () =>
-    props.theme === 'primary' ? '#09f' : '#274060'
+  const THEME = getThemeColor();
+  const STROKE_WIDTH = props.strokeWidth || 8;
+  const STROKE_COLOR = THEME;
 
-  const THEME = getThemeColor()
-  const STROKE_WIDTH = props.strokeWidth || 8
-  const STROKE_COLOR = THEME
+  const CENTER = props.size / 2;
+  const RADIUS = props.size / 2 - STROKE_WIDTH / 2;
+  const CIRCUMFERENCE = 2 * Math.PI * RADIUS;
 
-  const CENTER = props.size / 2
-  const RADIUS = props.size / 2 - STROKE_WIDTH / 2
-  const CIRCUMFERENCE = 2 * Math.PI * RADIUS
-
-  const [offset, setOffset] = useState(CIRCUMFERENCE)
+  const [offset, setOffset] = useState(CIRCUMFERENCE);
 
   useEffect(() => {
-    const progressOffset = ((100 - props.progress) / 100) * CIRCUMFERENCE
-    setOffset(progressOffset)
-  }, [setOffset, CIRCUMFERENCE, props.progress, offset])
+    const progressOffset = ((100 - props.progress) / 100) * CIRCUMFERENCE;
+    setOffset(progressOffset);
+  }, [setOffset, CIRCUMFERENCE, props.progress, offset]);
 
-
-  return <CC.Wrapper>
-    <CC.SvgWrapper style={{ width: props.size, height: props.size }}>
-      <CC.Svg width={props.size} height={props.size}>
-        <CC.CircleBG 
-          cy={CENTER}
-          cx={CENTER}
-          r={RADIUS}
-        />
-        <CC.Circle
-          fill= "none"
-
-          cy={CENTER}
-          cx={CENTER}
-          r={RADIUS}
-
-          stroke={STROKE_COLOR}
-          strokeWidth={STROKE_WIDTH}
-          strokeDasharray={CIRCUMFERENCE}
-          strokeDashoffset={offset}
-
-        />
-      </CC.Svg>
-      <CC.Percentage color={THEME}>
-        { props.progress }%
-      </CC.Percentage>
-    </CC.SvgWrapper>
-    {
-      props.caption && <CC.Caption>
-        { props.caption }
-      </CC.Caption>
-    }
-  </CC.Wrapper>
+  return (
+    <CC.Wrapper>
+      <CC.SvgWrapper style={{ width: props.size, height: props.size }}>
+        <CC.Svg width={props.size} height={props.size}>
+          <CC.CircleBG cy={CENTER} cx={CENTER} r={RADIUS} />
+          <CC.Circle
+            fill="none"
+            cy={CENTER}
+            cx={CENTER}
+            r={RADIUS}
+            stroke={STROKE_COLOR}
+            strokeWidth={STROKE_WIDTH}
+            strokeDasharray={CIRCUMFERENCE}
+            strokeDashoffset={offset}
+          />
+        </CC.Svg>
+        <CC.Percentage color={THEME}>
+          {Math.ceil(props.progress)}%
+        </CC.Percentage>
+      </CC.SvgWrapper>
+      {props.caption && <CC.Caption>{props.caption}</CC.Caption>}
+    </CC.Wrapper>
+  );
 }
 
-export default CircleChart
+export default CircleChart;
